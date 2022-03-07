e02b4b9cd4f46b5ec63188c601227cc8bb203268
yeonwoo <chiyy6162@gmail.com>
2022-03-07 22:12:10 +0900

    [feat] Route : useRouter 동적 매개변수값 추출 , Link 컴포넌트로 페이지링크 연결해보기

--- pages/news/[newsid].js
+++ pages/news/[newsid].js
@@ -1,6 +1,9 @@
 //domain/news/something-important
+import { useRouter } from "next/router";
 
 function DetailPage() {
+  const router = useRouter();
+  const newsId = router.query.newsId;
   return <h1>The Detail Page</h1>;
 }
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,21 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 //domain/news/news
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-js-a-great-framework">
+            NextJS Is a Great Framework
+          </Link>
+        </li>
+        <li>etc</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
