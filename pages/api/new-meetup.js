e45f8cd8693644da83ba0fbbe8de2a3bc7855dca
yeonwoo <chiyy6162@gmail.com>
2022-03-10 19:59:53 +0900

    [feat] database : mongoDB 이용하여 api 경로 http 요청 보내기

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://yeonwoo:2Kb2kuX9h8pRo5sf@cluster0.o3zk7.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
